e85113502c139fd7aaacf29b16afac71c7e118ff
Vincent1218 <vincentchai@hotmail.com>
2018-12-22 04:21:50 +0800

    Login Page

--- client/src/pages/login.vue
+++ client/src/pages/login.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="_login">
-    <div class="_container">
-      <div class="_block">体育处系统</div>
-      <div class="_block1">用科技贯穿校园</div>
-      <div class="_block2">创造一个数据驱动化的系统</div>
-      <div class="_block3">用户名</div>
-      <div class="_block4">密码</div>
+    <div class="container col-4">
+      <div class="block">体育处系统</div>
+      <div class="block1">用科技贯穿校园</div>
+      <div class="block2">创造一个数据驱动化的系统</div>
+      <div class="input-group">
+        <label class="block3" for="input-example-1">用户名</label>
+        <input class="block4" type="text" id="input-example-1" placeholder="请输入用户名">
+        <label class="block5" for="input-example-1">密码</label>
+        <input class="block6" type="password" id="input-example-1" placeholder="请输入密码">
+      </div>
+      <div class="form-group">
+        <label class="form-checkbox">
+          <input type="checkbox">
+          <i class="form-icon"></i> 记住我
+        </label>
+        <div class="forgot">忘记密码?</div>
+      </div>
+      <button class="btn btn-primary">登入</button>
     </div>
-    <div class="pic"></div>
+    <div class="pic col-8"></div>
   </div>
 </template>
 
