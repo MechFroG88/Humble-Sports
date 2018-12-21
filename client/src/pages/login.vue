f8c5fce3b6b818fe0e9cf737964947a9ecb6e110
Vincent1218 <vincentchai@hotmail.com>
2018-12-22 01:06:57 +0800

    hai

--- client/src/pages/login.vue
+++ client/src/pages/login.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="_login">Hi</div>
+  <div id="_login">
+    <div class="_container">
+      <div class="_block">体育处系统</div>
+      <div class="_block1">用科技贯穿校园</div>
+      <div class="_block2">创造一个数据驱动化的系统</div>
+      <div class="_block3">用户名</div>
+      <div class="_block4">密码</div>
+    </div>
+    <div class="pic"></div>
+  </div>
 </template>
 
 <script>
