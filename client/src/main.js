9102b9e1aabc374284dd95df2a3d05362ab9ee4a
Vincent1218 <vincentchai@hotmail.com>
2018-12-22 22:36:03 +0800

    Login Page

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './assets/scss/style.scss';
-import '../node_modules/spectre.css/src/spectre.scss';
-import '../dist/icons/style.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/scss/style.scss";
+import "../node_modules/spectre.css/src/spectre.scss";
+// import '../dist/icons/style.css';
 
 Vue.config.productionTip = false;
 
