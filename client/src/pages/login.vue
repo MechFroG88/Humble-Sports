5d01a87f51aca9a0ee1cf662f201134bafe086a3
DavidTan0527 <david_weiucheng@hotmail.com>
2018-12-23 11:15:55 +0800

    Update login page

--- client/src/pages/login.vue
+++ client/src/pages/login.vue
@@ -7,8 +7,8 @@
       <div class="input-group">
         <label class="block3" for="input-example-1">用户名</label>
         <input class="block4" type="text" id="input-example-1" placeholder="请输入用户名">
-        <label class="block5" for="input-example-1">密码</label>
-        <input class="block6" type="password" id="input-example-1" placeholder="请输入密码">
+        <label class="block5" for="input-example-2">密码</label>
+        <input class="block6" type="password" id="input-example-2" placeholder="请输入密码">
       </div>
       <div class="form-group">
         <label class="form-checkbox">
@@ -17,7 +17,7 @@
         </label>
         <div class="forgot">忘记密码?</div>
       </div>
-      <button class="btn btn-primary">登入</button>
+      <button class="btn btn-primary" type="submit" @click="$router.push('/rent')">登入</button>
     </div>
     <div class="pic col-8"></div>
   </div>
