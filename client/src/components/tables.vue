9854b1d8b67ccb0caf9614ef8638538d97d8e39a
DavidTan0527 <david_weiucheng@hotmail.com>
2018-12-23 12:30:38 +0800

    Add table component

---
+++ client/src/components/tables.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="_table">
+    <table :style="`width:${width};`" :class="{
+      'striped': striped,
+      'hoverable': hoverable
+    }">
+      <tr class="table_columnns">
+        <th v-for="column in columns" :key="column.name"
+        :class="`col_${column.name}`">{{column.name}}</th>
+      </tr>
+      <tr v-for="(row, row_num) in data" :key="row_num">
+        <td v-for="index in columns.length" :key="index"
+        :class="`col_${columns[index-1].field}`">
+          {{row[columns[index - 1].field]}}
+          <slot :name="columns[index-1].field" v-if="!row[columns[index-1].field]"/>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    columns: Array,
+    data: Array,
+    width: String,
+    striped: Boolean,
+    hoverable: Boolean,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/pages/rent/personal.vue
+++ client/src/pages/rent/personal.vue
@@ -1,10 +1,47 @@
 <template>
-  
+  <div id="_personal">
+    <cp-table width="100%" :columns="columns" :data="data">
+      <div slot="info">
+        asdhasdhasdhasd
+      </div>
+    </cp-table>
+  </div>
 </template>
 
 <script>
+import cpTable from '@/components/tables'
 export default {
-
+  components: {
+    cpTable
+  },
+  data: () => ({
+    columns: [
+      { name: '学生资料', field: 'info'        },
+      { name: '借用器材', field: 'item'        },
+      { name: '借出时间', field: 'rent_time'   },
+      { name: '归还时间', field: 'return_time' },
+      { name: '负责老师', field: 'teacher'     },
+      { name: '状态'   , field: 'status'      }
+    ],
+    data: [
+      {
+        item: 'b',
+        // info: 'a',
+        rent_time: 'c',
+        return_time: 'd',
+        teacher: 'e',
+        status: 'f'
+      },
+      {
+        info: '1',
+        item: '2',
+        rent_time: '3',
+        return_time: '4',
+        teacher: '5',
+        status: '6'
+      }
+    ]
+  })
 }
 </script>
 
