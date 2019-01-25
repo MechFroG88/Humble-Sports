c79de496b1a1e5053739b157d993762cabd3049f
MechFroG88 <kelzzin2@gmail.com>
2019-01-25 18:52:33 +0800

    Update Database Migration

---
+++ application/database/migrations/2019_01_20_060655_personal_rents.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PersonalRents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_rents', function (Blueprint $table) {
+            $table->increment('id');
+            $table->integer('student_id');
+            $table->integer('item_id');
+            $table->integer('item_tag');
+            $table->datetime('item_out');
+            $table->datetime('item_in');
+            $table->datetime('duedate');
+            $table->tinyInteger('status');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExits('personal_rents');
+    }
+}
