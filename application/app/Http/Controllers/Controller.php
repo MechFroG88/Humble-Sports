b3377d125a693117dda6fec0dc06f48603a18419
MechFroG88 <kelzzin2@gmail.com>
2019-02-03 00:56:52 +0800

    Update User Model and Controller

--- application/app/Http/Controllers/Controller.php
+++ application/app/Http/Controllers/Controller.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function fail()
+    {
+        return response("Validation error",400);
+    }
+
+    protected function ok()
+    {
+        return response("Successful operation",200);
+    }
 }
