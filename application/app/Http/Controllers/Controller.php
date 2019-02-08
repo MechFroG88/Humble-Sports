ff3bccbcfd5eb6a15fe88b1a5c4557b04f435e7c
MechFroG88 <kelzzin2@gmail.com>
2019-02-08 22:31:17 +0800

    Update Student Model and Contoller

--- application/app/Http/Controllers/Controller.php
+++ application/app/Http/Controllers/Controller.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
-use Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
--- application/app/Student.php
+++ application/app/Student.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $primaryKey = "id"; 
-    public $incrementing  = false;
-    protected $hidden     = ['created_at','updated_at'];
-
-    public function personalrent(){
-        return $this->hasMany('App\PersonalRent');
-    }
-    public function grouprent(){
-        return $this->hasMany('App\GroupRent');
-    }
+    public $incrementing = false;
+    protected $visible = ['id','cn_name', 'type'];
 }
