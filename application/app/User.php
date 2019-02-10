a28482213a5dc8d541247585e8ef6bc481a9885f
Shancute <jinshancrayon@gmail.com>
2019-02-10 16:19:06 +0800

    Update User Model and Controller

--- application/app/User.php
+++ application/app/User.php
@@ -10,5 +10,11 @@ class User extends Authenticatable
 {
     use SoftDeletes;
     protected $dates = ['deleted_at'];
+    protected $fillable = ['username','cn_name','password','type'];
     protected $visible = ['id','cn_name', 'type'];
+
+    public function receipt(){
+        return $this->hasMany('App/Receipt');
+    }
+
 }
