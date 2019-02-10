17488b917043a5b98c487bfc5fb15bf1b953e263
Shancute <jinshancrayon@gmail.com>
2019-02-10 16:19:06 +0800

    Update User Model and Controller

--- application/app/User.php
+++ application/app/User.php
@@ -8,23 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+    protected $fillable = ['username','cn_name','password','type'];
+    protected $visible = ['id','cn_name', 'type'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public function receipt(){
+        return $this->hasMany('App/Receipt');
+    }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 }
