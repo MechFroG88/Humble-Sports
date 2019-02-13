2d93e79408a42ed498c21d5b82a6c57d7927a164
Shancute <jinshancrayon@gmail.com>
2019-02-13 16:40:26 +0800

    Update Group Receipt Model

--- application/app/GroupReceipt.php
+++ application/app/GroupReceipt.php
@@ -11,13 +11,13 @@ use App\Fine;
 
 class GroupReceipt extends Model
 {
-    protected $visible = ['single_fine','fine','user','grouprent','type'];
+    protected $visible = ['single_fine','single_price','fine','user','grouprent','type'];
 
     public function item(){
         return $this->belongsTo('App\Item');
     }
     public function user(){
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User')->select(array('id','cn_name'));
     }
     public function grouprent(){
         return $this->belongsTo('App\GroupRent');
