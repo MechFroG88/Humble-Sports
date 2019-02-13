06690330995cc07e65add0e1045642620ca9edb2
Shancute <jinshancrayon@gmail.com>
2019-02-13 09:57:31 +0800

    Update Student Model and Controller

--- application/app/Student.php
+++ application/app/Student.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $primaryKey = "id"; 
-    public $incrementing = false;
-    protected $visible = ['id','cn_name', 'type'];
+    public $incrementing  = false;
+    protected $hidden     = ['created_at','updated_at'];
+
+    public function personalrent(){
+        return $this->hasMany('App\PersonalRent');
+    }
+    public function grouprent(){
+        return $this->hasMany('App\GroupRent');
+    }
 }
