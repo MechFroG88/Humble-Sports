4e85a763cd5cf8ff6f7e8e68305e1c05c02254fa
Shancute <jinshancrayon@gmail.com>
2019-02-13 10:24:10 +0800

    Update Controller

--- application/app/Http/Controllers/Controller.php
+++ application/app/Http/Controllers/Controller.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
