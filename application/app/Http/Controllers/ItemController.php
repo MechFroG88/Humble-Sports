4251f20b0ddf92fedcd1fb685c5d694d08f1c60b
Shancute <jinshancrayon@gmail.com>
2019-02-13 10:22:32 +0800

    Update Item Model and Controller

--- application/app/Http/Controllers/ItemController.php
+++ application/app/Http/Controllers/ItemController.php
@@ -15,6 +15,7 @@ class ItemController extends Controller
         "start_id" => "required|integer",
         "end_id"   => "required|integer",
         "price"    => "required|integer",
+        "title"    => ""
     ];
 
     public function create(Request $data)
@@ -33,15 +34,9 @@ class ItemController extends Controller
 
     public function get_single($id)
     {
-        $item = item::where('id', $id)->get()->first();
-        return response($item->toJson(),200);
-    }
-
-    public function get_single_rent($id)
-    {
         $personalrent = personalrent::where('item_id',$id)->get();
         $grouprent    = grouprent::where('item_id',$id)->get()->makeHidden(['cn_name','class','phone_no']);
-        $merged       = $personalrent->merge($grouprent)->makeHidden(['item_tag','amount','lost','due_date','returned']);
+        $merged       = $personalrent->merge($grouprent);
         return response($merged->toJson(),200);
     }
 }
