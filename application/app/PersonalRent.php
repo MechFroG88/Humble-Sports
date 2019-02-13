a00bf0b8ad95df2a4c7f43d5cdde8ddac553cf7f
Shancute <jinshancrayon@gmail.com>
2019-02-13 10:21:48 +0800

    Update PersonalRent  Model and Controller

---
+++ application/app/PersonalRent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+use App\PersonalReceipt;
+use App\Student;
+use App\item;
+
+
+class PersonalRent extends Model
+{
+    use SoftDeletes;
+    public $timestamps  = false;
+    protected $hidden   = ['id','deleted_at','due_date','item_tag','student_id','item_id'];
+    protected $fillable = ['teacher','student_id','item_out','due_date','item_id','item_tag'];
+    protected $dates    = ["deleted_at","item_in","item_out","due_date"];
+
+    public function student(){
+        return $this->belongsTo('App\Student');
+    }
+    public function item(){
+        return $this->belongsTo('App\Item');
+    }
+    public function personalreceipt(){
+         return $this->hasOne('App\PersonalReceipt');
+    }   
+}
