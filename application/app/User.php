4d6f6480ef23b3b22611eae0b230213cca6c755a
Shancute <jinshancrayon@gmail.com>
2019-02-13 09:58:11 +0800

    Update User Model and Controller

--- application/app/User.php
+++ application/app/User.php
@@ -2,16 +2,16 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
     use SoftDeletes;
-    protected $dates = ['deleted_at'];
+    protected $dates    = ['deleted_at'];
     protected $fillable = ['username','cn_name','password','type'];
-    protected $visible = ['id','cn_name', 'type'];
+    protected $visible  = ['id','cn_name', 'type'];
 
     public function receipt(){
         return $this->hasMany('App/Receipt');
