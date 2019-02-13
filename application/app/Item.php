bb7ac607bdf6f01c3c5637eff3acf0bbfaefbd06
Shancute <jinshancrayon@gmail.com>
2019-02-13 10:22:32 +0800

    Update Item Model and Controller

---
+++ application/app/Item.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Item;
+
+class Item extends Model
+{
+    protected $visible  = ['id','type','start_id','end_id','price'];
+    protected $fillable = ['type','start_id','end_id','price','title'];
+
+    public function personalrent(){
+         return $this->hasMany('App\PersonalRent');
+    }
+
+    public function grouprent(){
+        return $this->hasMany('App\GroupRent');
+    }
+}
