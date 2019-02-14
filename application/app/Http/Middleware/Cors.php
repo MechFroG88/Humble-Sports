cf87d822eef5a7e3dfd306833674be40c79466f6
Shancute <jinshancrayon@gmail.com>
2019-02-14 18:53:52 +0800

    Add Cors Middleware

---
+++ application/app/Http/Middleware/Cors.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+        //->header('Access-Control-Allow-Origin', 'http://localhost:8080')
+        //->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        //->header('Access-Control-Allow-Headers', 'authorization, X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding');
+    }
+}
+
