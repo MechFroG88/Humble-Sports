5df01a4a092431547be0d0add481cddf6f4a4831
DavidTan0527 <david_weiucheng@hotmail.com>
2019-02-18 22:38:45 +0800

    Change eslint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,6 +10,20 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'camel-case': {
+      'properties': 'never'
+    },
+    'prefer-const': ['error', {
+      'destructuring': 'any',
+      'ignoreReadBeforeAssign': false
+    }],
+    'sort-imports': ['error', {
+      'ignoreCase': true,
+      'ignoreDeclarationSort': false,
+      'ignoreMemberSort': false,
+      'memberSyntaxSortOrder': ['none', 'all', 'single', 'multiple']
+    }],
+    'indent': ['error', 2]
   },
   parserOptions: {
     parser: 'babel-eslint',
