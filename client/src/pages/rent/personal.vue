b4d2300e65cc20a1320590aa14e86769c3fa49c8
DavidTan0527 <david_weiucheng@hotmail.com>
2019-02-18 22:48:45 +0800

    Update personal rental page

--- client/src/pages/rent/personal.vue
+++ client/src/pages/rent/personal.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="_personal">
-    <div class="btn btn-lg btn-primary" 
+    <div class="btn btn-lg btn-primary"
     @click="$router.push({
       'name': 'addNew',
       'params': {
@@ -20,6 +20,16 @@
         </div>
       </template>
 
+      <template slot="item_out" slot-scope="{ data }">
+        <div class="date">{{toDate(data.item_out)}}</div>
+        <div class="time">{{toTime(data.item_out)}}</div>
+      </template>
+
+      <template slot="item_in" slot-scope="{ data }">
+        <div class="date">{{toDate(data.item_in)}}</div>
+        <div class="time">{{toTime(data.item_in)}}</div>
+      </template>
+
       <template slot="status" slot-scope="{ data }">
           <span v-if="data.status == 0" class="label label-success">已归还</span>
           <span v-if="data.status == 1" class="label label-primary">未归还</span>
@@ -31,34 +41,48 @@
 </template>
 
 <script>
-import { getPersonalRent } from '@/api/rental'
+import { getPersonalRent } from '@/api/rental';
 
-import cpTable from '@/components/tables'
-import cmodal from '@/components/confirm-modal'
-import { personal_column } from '@/api/tableColumns'
+import cpTable from '@/components/tables';
+import cmodal from '@/components/confirm-modal';
+import { personal_column } from '@/api/tableColumns';
 // import { personal } from '@/api/mock/rental'
 export default {
   components: {
     cpTable,
-    cmodal
+    cmodal,
   },
   data: () => ({
     columns: personal_column,
     data: [],
   }),
   mounted() {
-    getPersonalRent().then(({data}) => {
+    getPersonalRent().then(({ data }) => {
       this.data = data;
       for (let i = 0; i < this.data.length; i++) {
-        this.data[i].item_type = this.data[i].item.type
+        this.data[i].item_type = this.data[i].item.type;
+        this.data[i].item_in_date = `${this.data[i].item_in.split(' ')[0].split('-')[0]}年${this.data[i].item_in.split(' ')[0].split('-')[1]}月${this.data[i].item_in.split(' ')[0].split('-')[2]}日`;
       }
-      this.$refs.table.isLoading = false
-      console.log(this.data)
+      this.$refs.table.isLoading = false;
     }).catch((err) => {
-      console.log(err)
-    })
-  }
-}
+      console.log(err);
+    });
+  },
+  methods: {
+    toDate(date) {
+      return `${date.split(' ')[0].split('-')[0]} 年 ${parseInt(date.split(' ')[0].split('-')[1])} 月 ${parseInt(date.split(' ')[0].split('-')[2])} 日`;
+    },
+    toTime(date) {
+      let time = '上午';
+      const times = date.split(' ')[1].split(':');
+      if (times[0] >= 12) {
+        time = times[0] == 12 ? '中午' : '下午';
+        times[0] -= 12;
+      }
+      return `${time}${times[0]}：${times[1]}`;
+    },
+  },
+};
 </script>
 
 <style>
