013ac38e331d345ca3d5e1b0e2793349d179ba5b
Shancute <jinshancrayon@gmail.com>
2019-02-26 22:01:06 +0800

    Update Database Migration

--- application/database/migrations/2019_01_21_155041_group_receipts.php
+++ application/database/migrations/2019_01_21_155041_group_receipts.php
@@ -17,9 +17,9 @@ class GroupReceipts extends Migration
             $table->increments('id');
             $table->integer('grouprent_id');
             $table->integer('user_id');
-            $table->integer('single_price');
-            $table->integer('single_fine');
-            $table->integer('null');
+            $table->integer('single_price')->nullable();
+            $table->integer('single_fine')->nullable();
+            $table->integer('total')->default(0);
             $table->tinyInteger('type');
             $table->timestamps();
         });    //
