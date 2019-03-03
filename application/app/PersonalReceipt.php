5d1ee813438dfd904794cdcb223b6d76a519cac7
MechFroG88 <kelzzin2@gmail.com>
2019-03-03 21:03:56 +0800

    Update Backend

--- application/app/PersonalReceipt.php
+++ application/app/PersonalReceipt.php
@@ -11,7 +11,7 @@ use App\Fine;
 
 class PersonalReceipt extends Model
 {
-    protected $visible = ['single_fine','fine','user','personalrent','type'];
+    protected $visible = ['id','single_fine','fine','user','personalrent','type'];
 
     public function item(){
         return $this->belongsTo('App\Item','price');
