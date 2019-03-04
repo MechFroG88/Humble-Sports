f4cb25240a6cdc8e75c912fde156acf98be87fb4
MechFroG88 <kelzzin2@gmail.com>
2019-03-04 21:26:55 +0800

    Update Database Migration

--- application/database/migrations/2019_01_20_061301_personal_receipts.php
+++ application/database/migrations/2019_01_20_061301_personal_receipts.php
@@ -15,6 +15,7 @@ class PersonalReceipts extends Migration
     {
         Schema::create('personal_receipts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('personalrent_id');
             $table->integer('user_id');
             $table->integer('total')->default(0);
             $table->integer('days')->default(0);
