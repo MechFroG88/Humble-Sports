20eb30516d108e03613fdb74e80d2d8a3acf5203
MechFroG88 <kelzzin2@gmail.com>
2019-03-16 17:39:29 +0800

    Update Database Seeder

--- application/database/seeds/UsersTableSeeder.php
+++ application/database/seeds/UsersTableSeeder.php
@@ -11,18 +11,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        DB::table('users')->insert([[
             'username' => 'admin',
             'cn_name' => '管理员',
             'password' => Hash::make('admin'),
             'type' => 0,
+            'created_at' => date('Y-m-d H:i:s')
         ],
         [
             'username' => 'david',
             'cn_name' => '大卫',
             'password' => Hash::make('david'),
             'type' => 0,
+            'created_at' => date('Y-m-d H:i:s')
         ]
-        );
+        ]);
     }
 }
