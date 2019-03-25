4171c7cceae56ce472f278adb8b07cb1ee5326cf
MechFroG88 <kelzzin2@gmail.com>
2019-03-25 23:59:17 +0800

    Update Seeder

--- application/database/seeds/DatabaseSeeder.php
+++ application/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(FineTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            FineTableSeeder::class
+        ]);
     }
 }
