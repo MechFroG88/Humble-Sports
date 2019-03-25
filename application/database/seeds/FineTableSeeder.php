3f83d729229b891f1a5b3b853cc06cfc672598a0
MechFroG88 <kelzzin2@gmail.com>
2019-03-25 23:55:52 +0800

    Update Seeder

---
+++ application/database/seeds/FineTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FineTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('fines')->insert([
+            'fine' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
