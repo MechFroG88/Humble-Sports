ee50d7649c38431e86390ebabf1c07f217622d6a
Tex Tang <tex@Texs-MacBook-Pro-3.local>
2019-03-26 14:59:38 +0800

    Update Backend

--- application/app/Http/Controllers/PersonalReceiptController.php
+++ application/app/Http/Controllers/PersonalReceiptController.php
@@ -64,6 +64,6 @@ class PersonalReceiptController extends Controller
         if (!isset($receipt)){
             $receipt = collect();
         }
-        return $receipt;
+        return response($groupreceipt->toJson(),200);
     }
 }
