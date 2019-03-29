52f9a0d7247860724a174b37864c1da21c312d71
DavidTan0527 <david_weiucheng@hotmail.com>
2019-03-29 16:54:01 +0800

    Add vee validate

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,15 @@ import store from './store';
 import '../node_modules/spectre.css/src/spectre.scss';
 import './assets/scss/style.scss';
 import './dist/icons/style.css';
-import 'vue-flash-message/dist/vue-flash-message.min.css';
+
+import VeeValidate, { Validator } from 'vee-validate';
+import { errorBagConfig, errorMessage, errorMessageGlobal } from './validation';
+import zh_CN from 'vee-validate/dist/locale/zh_TW';
+Vue.use(VeeValidate, errorBagConfig);
+Validator.localize('zh_CN', zh_CN);
 
 import VueFlashMessage from 'vue-flash-message';
+import 'vue-flash-message/dist/vue-flash-message.min.css';
 Vue.use(VueFlashMessage, { 
   method: 'notification',
   messageOptions: {
